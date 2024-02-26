b2bfc89851f9982f0ae41f392b24b633d49f4431
haroondhanyal <rajahroon235@gmail.com>
2024-02-26 15:23:04 +0500

    Update the XPath fro 2nd Test Casese

--- cypress/e2e/TestCases/LoginScreen/Login_without.cy.js
+++ cypress/e2e/TestCases/LoginScreen/Login_without.cy.js
@@ -1,6 +1,8 @@
 /// <reference types="Cypress"/>
 
-import { HomePage_without_Login } from "../../Pages/Without_User_Login/Home_Page";
+import {
+    HomePage_without_Login
+} from "../../Pages/Without_User_Login/Home_Page";
 import HomePageElementsLocators from "../../Locators/HomePage/Main_Home_Page.json";
 
 
@@ -15,9 +17,9 @@ const Main_Page = new HomePage_without_Login
 
 describe('Visit RealtorsPK Website', () => {
     it('Should visit RealtorsPK website', () => {
-       
+
         Cypress.on('uncaught:exception', () => {
-           
+
             return false;
         });
 
@@ -26,39 +28,39 @@ describe('Visit RealtorsPK Website', () => {
             cy.visit('https://dev.realtorspk.com');
             cy.wait(5000);
 
-            cy.fixture('HomePage_Search').then((data)=>{
+            cy.fixture('HomePage_Search').then((data) => {
                 Main_Page.city_selected(data.city_2);
                 cy.wait(4000);
-               
-                Main_Page.budget_selection(data.min_price,data.max_price)
-                cy.wait(4000);             
+
+                Main_Page.budget_selection(data.min_price, data.max_price)
+                cy.wait(9000);
                 const propertyTypeXPathsToCheck = [
-                    
+
                     HomePageElementsLocators.Main_Home_Page_Locators.house_villa,
-                    HomePageElementsLocators.Main_Home_Page_Locators.flat,                 
+                    HomePageElementsLocators.Main_Home_Page_Locators.appartment,
                     HomePageElementsLocators.Main_Home_Page_Locators.pent_house,
                     HomePageElementsLocators.Main_Home_Page_Locators.farm_house,
                     HomePageElementsLocators.Main_Home_Page_Locators.basement,
-                    // HomePageElementsLocators.Main_Home_Page_Locators.plot,
+                    HomePageElementsLocators.Main_Home_Page_Locators.plot,
                     HomePageElementsLocators.Main_Home_Page_Locators.upper_portion,
-                    HomePageElementsLocators.Main_Home_Page_Locators.room,
-                    
+                    HomePageElementsLocators.Main_Home_Page_Locators.ground_floor,
+
                     // Add other XPaths as needed
                 ];
                 Main_Page.property_types(propertyTypeXPathsToCheck);
                 cy.wait(3000);
-                        
+
                 Main_Page.search_button();
                 cy.wait(4000);
-        
-                })
 
-                      
+            })
+
+
             cy.wait(2000);
         } catch (error) {
-         
-            console.error('Error occurred will be:', error.message); 
-            cy.log('Error occurred will be: ' + error.message); 
+
+            console.error('Error occurred will be:', error.message);
+            cy.log('Error occurred will be: ' + error.message);
         }
     });
 });
